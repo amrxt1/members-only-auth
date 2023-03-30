06e6f942ae5744af203880fb8b0c3629af4004a8
amrxt1 <amrxt11@outlook.com>
2023-03-30 10:23:36 +0530

    Create Post and Member models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+    def new
+        @post = Post.new
+    end
+    def create
+    end
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :members
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "posts#index"
+  resources :posts, only: [:index,:new,:create]
+
+
 end
