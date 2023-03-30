b27a09179dc92f35d1e713f9e6bbfd5015299866
amrxt1 <amrxt11@outlook.com>
2023-03-30 10:42:20 +0530

    Make the form and #create action for a new POst

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,25 @@
 class PostsController < ApplicationController
+
+    #before_action :authenticate_member!, only: [:new, :create]
+
     def new
         @post = Post.new
     end
     def create
+        @post = Post.new(post_params)
+        @post.member_id = current_user.id
+
+        if @post.save
+            redirect_to root_url
+        else
+            render :new
+        end
     end
     def index
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title,:body)
     end
 end
